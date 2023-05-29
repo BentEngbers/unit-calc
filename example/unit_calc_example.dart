074101bf8e856490a0dc17978dbd148fc650bf0b
Bent Engbers <bentengbers@gmail.com>
2023-05-29 12:30:35 +0200

    initial dart project

---
+++ example/unit_calc_example.dart
@@ -0,0 +1,6 @@
+import 'package:unit_calc/unit_calc.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/unit_calc_test.dart
@@ -0,0 +1,16 @@
+import 'package:unit_calc/unit_calc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
